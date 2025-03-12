79dad872c8ac4f2a3b9aeb65dbda8437671aefc4
Lopexissimo <101009904+Lopexissimo@users.noreply.github.com>
2025-03-12 12:30:05 +0100

    src/App.js

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,20 +1,59 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const Api={
-    getUsers(){
-        const url='http://kg00w4owgo0o8okwgk4w4g40.185.229.236.238.sslip.io/users'
-        const params = {
-            page:1,
-            pagesize:10
+const Api = {
+    basicUrl: 'http://kg00w4owgo0o8okwgk4w4g40.185.229.236.238.sslip.io/users',
+
+    async getUsers() {
+        try {
+            const response = await axios.get(this.basicUrl, {
+                params: { page: 1, pagesize: 3 }
+            });
+            return response.data.results;
+
+        } catch (err) {
+            console.error("Errore API:", err);
+            return null;
         }
-        axios.get(url, {params})
-        .then( response => {
-            return response;
-        }).catch(err => {
-            alert(err);
-        })
-    }
+    },
 
-}
+    async getUsersById(id) {
+        try {
+            const response = await axios.get(`${this.basicUrl}/${id}`);
+            return response.data.results;
+        } catch (err) {
+            console.error(`Errore caricamento user con ID: ${id}`, err);
+            return null;
+        }
+    },
+    async addUser(user) {
+        try {
+            const response = await axios.post(this.basicUrl, user)
+            return response.data
+        } catch (err) {
+            console.error(`Errore creazione user con ID: ${user.id}`, err);
+            return null;
+        }
+    },
+
+    async editUser(id, editedUser) {
+        try {
+            const response = await axios.put(`${this.basicUrl}/${id}`, editedUser)
+            return response.data;
+        } catch (err) {
+            console.error(`Errore modifica user con ID: ${id}`, err);
+            return null;
+        }
+    },
+
+    async deleteUser(id) {
+        try {
+            const response = await axios.delete(`${this.basicUrl}/${id}`)
+            return true;
+        } catch (err) {
+            console.error(`Errore eliminazione user con ID: ${id}`, err);
+            return false;
+        }
+    }
+};
 
 export default Api;
